fd20a87d82b101a47eea03df1b2811e40a5d922e
Allen-Josu <allenalackaparambil@gmail.com>
2025-01-03 14:31:47 +0530

    tacked

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import GoogleLogin, { GoogleLogout } from "react-google-login"
+
+const clientId = "1067183531624-6fdo6qjmu4h748peb0asftdbk2ecvkit.apps.googleusercontent.com"
+
 
 export default function Home() {
+  const onSuccess = (response) => {
+    console.log('Login Success:', response.profileObj);
+  };
+
+  const onFailure = (response) => {
+    console.log('Login Failed:', response);
+  };
+
+  const onLogoutSuccess = () => {
+    console.log('Logout Successful');
+  };
   return (
     <>
     <p>my Home</p>
+  
     </>
   )
 }
